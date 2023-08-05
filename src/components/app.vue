43249304351593645a14ba83431511ebbd943518
Shachar Itzhaky <corwin.amber@gmail.com>
2023-08-05 16:17:20 +0300

    [refactor] Switch to vue-facing-decorator.
    Using class components. This is much cleaner.

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,16 +1,22 @@
 <template>
-  <notebook :model="{cells: []}"/>
+    <notebook :model="model"/>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue, toNative } from 'vue-facing-decorator';
+import { NotebookApp } from '../notebook_model';
 import Notebook from './notebook.vue';
 
-export default {
-    // name: "AppComponent",
-    components: {Notebook}
+@Component({
+  components: { Notebook }
+})
+class App extends Vue {
+    model: NotebookApp.Model
+
+    created() {
+        this.model = {cells: [{kind: 'code', input: 'abc'}]};
+    }
 }
+
+export default toNative(App);
 </script>
-
-<style scoped>
-
-</style>
