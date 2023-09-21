e96cd3d24c32db1c932ef1607f3d8fb23b1d3968
Oren Afek <oren.afek@gmail.com>
2023-09-21 16:36:28 +0300

    Add loading spinner to Cell's model

---
+++ src/components/loading-spinner/loading-spinner.vue
@@ -0,0 +1,46 @@
+<template>
+    <button ref="btn" @click="submitAction" :style="{minWidth}">
+        <span class="caption" :class="{isLoading}"><slot></slot></span>
+        <spinner-anim v-if="isLoading"
+                      style="--size: 20px; display: inline-block; vertical-align: middle"/>
+    </button>
+</template>
+
+<style scoped>
+button {
+    padding-top: 0;
+    padding-bottom: 0;
+    height: 23px;
+}
+
+span.caption.isLoading {
+    display: none;
+}
+</style>
+
+<script lang="ts">
+// @ts-ignore
+import SpinnerAnim from './spinner-anim.vue';
+import {Component, Prop, toNative, Vue} from "vue-facing-decorator";
+
+@Component({components: {SpinnerAnim}, 'emits': ['loadingButtonClick']})
+class LoadingSpinner extends Vue {
+
+    @Prop
+    isLoading: Boolean
+
+    minWidth: string
+
+    $refs: { btn: HTMLButtonElement }
+
+    mounted() {
+        this.minWidth = `${this.$refs.btn.getBoundingClientRect().width}px`;
+    }
+
+    async submitAction() {
+        this.$emit('loadingButtonClick');
+    }
+}
+
+export default toNative(LoadingSpinner);
+</script>
