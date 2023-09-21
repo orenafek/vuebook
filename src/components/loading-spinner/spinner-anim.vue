3c53ed29c81581e23e85b73b59995c1fe04ec048
Oren Afek <oren.afek@gmail.com>
2023-09-21 16:36:28 +0300

    Add loading spinner to Cell's model

---
+++ src/components/loading-spinner/spinner-anim.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="semipolar-spinner" style="--nrings: 4; --time-shift: 0.05">
+      <div class="ring"></div>
+      <div class="ring"></div>
+      <div class="ring"></div>
+      <div class="ring"></div>
+  </div>
+</template>
+
+<style src="./epic-spinners.css"></style>
+
+<script>
+export default { }
+</script>
